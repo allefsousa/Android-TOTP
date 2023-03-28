a2acd20fc121399722ebe9b8bd3303d6f23cf3b8
Allef Sousa <allefsousa_1@hotmail.com>
2023-03-28 19:20:03 -0300

    Adding layout

--- app/src/main/java/com/developer/allefsousa/totop/GeneratorActivity.kt
+++ app/src/main/java/com/developer/allefsousa/totop/GeneratorActivity.kt
@@ -3,6 +3,7 @@ package com.developer.allefsousa.totop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import com.developer.allefsousa.toptp.TotpAuthenticator
 import com.developer.allefsousa.totop.databinding.ActivityGeneratorBinding
 import java.net.Authenticator
@@ -18,5 +19,31 @@ class GeneratorActivity : AppCompatActivity(R.layout.activity_generator) {
 
         val authenticator = TotpAuthenticator(SECRET.toByteArray())
         Log.d(GeneratorActivity::class.java.name, "onCreate: ${authenticator.generate()}")
+
+        binding.btnGenerate.setOnClickListener {
+            generated(authenticator)
+        }
+
+        binding.secret.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                generated(authenticator)
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+
+
+    }
+
+    private fun generated(authenticator: TotpAuthenticator) {
+        if (binding.secret.text!!.isEmpty()) {
+            binding.textSecret.text = "Secret: ${SECRET}"
+            binding.textCode.text = "TOTP Code: ${authenticator.generate()}"
+        } else {
+            val newAuth = TotpAuthenticator(binding.secret.text.toString().toByteArray())
+            binding.textSecret.text = "Secret: ${binding.secret.text.toString()}"
+            binding.textCode.text = "TOTP Code: ${newAuth.generate()}"
+            binding.secret.text!!.clear()
+        }
     }
 }
